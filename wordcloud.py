6688c6d8109ebd9efdad932e93817bc138da1130
Toomore Chiang <toomore0929@gmail.com>
2011-05-30 18:24:00 +0800

    Add calfre().

--- wordcloud.py
+++ wordcloud.py
@@ -1,7 +1,10 @@
+import operator
+
 class wordcloud(object):
   def __init__(self, w, l = [2,3,4]):
     self.word = w.decode('utf-8')
     self.l = l
+    self.makelist()
 
   def getlens(self):
     return len(self.word)
@@ -18,3 +21,13 @@ class wordcloud(object):
           self.ml.append(self.word[0+add-i:add])
           add += 1
     return self.ml
+
+  def calfre(self):
+    re = {}
+    for i in self.ml:
+      try:
+        re[i] += 1
+      except KeyError:
+        re[i] = 1
+    x = sorted(re.iteritems(), key=operator.itemgetter(1),reverse=True)
+    return x
