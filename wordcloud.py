e624ba4fe6b5e340d4a9b6bf34d6a551eba55c92
Toomore Chiang <toomore0929@gmail.com>
2011-06-02 11:04:49 +0800

    Add maketimesdict.

--- wordcloud.py
+++ wordcloud.py
@@ -5,9 +5,13 @@ class wordcloud(object):
     self.word = w.decode('utf-8')
     self.l = l
     self.makelist()
+    self.calfres = self.calfre()
 
   def getlens(self):
     return len(self.word)
+
+  def getwords(self):
+    return self.calwords.keys()
 
   def cklens(self, l):
     return len(self.word) >= l
@@ -29,5 +33,16 @@ class wordcloud(object):
         re[i] += 1
       except KeyError:
         re[i] = 1
+    self.calwords = re
     x = sorted(re.iteritems(), key=operator.itemgetter(1),reverse=True)
     return x
+
+  def maketimesdict(self):
+    re = {}
+    for i in self.calwords:
+      try:
+        re[self.calwords[i]].append(i)
+      except KeyError:
+        re[self.calwords[i]] = []
+        re[self.calwords[i]].append(i)
+    return re
